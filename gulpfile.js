698949f97be320d8d5a4617d0ebc7d71dda5c269
Radek Palisa <radek.palisa@gmail.com>
2016-11-12 12:24:47 +0100

    adds gulp task runner and underscores css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
+var sass = require("gulp-sass");
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -9,18 +9,42 @@ gulp.task('serve', ['sass'], function() {
         server: "./"
     });
 
-    gulp.watch(["./sass/*.scss", "./sass/*.sass"], ['sass']);
+    gulp.watch(["./sass/**/*.scss", "./sass/**/*.sass"], ['sass']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
+
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
   return gulp.src('./sass/style.scss')
-  	.pipe(sass().on('error', sass.logError))
+  	.pipe(sass().on('error', function (err) {
+            console.error('Error!', err.message);
+    }))
     .pipe(gulp.dest('./css/'))
     .pipe(browserSync.stream());
 });
 
+
+/**
+ * Compile with gulp-ruby-sass + source maps
+
+gulp.task('sass', function () {
+
+    return sass('./sass', {sourcemap: true})
+        .on('error', function (err) {
+            console.error('Error!', err.message);
+        })
+        .pipe(sourcemaps.write('./sass', {
+            includeContent: false,
+            sourceRoot: '/sass'
+        }))
+        .pipe(browserSync.stream({match: '**//*.css'}));
+});
+*/
+
+/*
 gulp.task('default', function() {
   // place code for your default task here
 });
+*/
+gulp.task('default', ['serve']);
