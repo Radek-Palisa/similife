c331799d7d96703557a6431687e11d94ea5e1595
Radek Palisa <radek.palisa@gmail.com>
2016-11-27 11:33:37 +0100

    sticky secondary menu

--- scripts/custom.js
+++ scripts/custom.js
@@ -7,23 +7,43 @@ var ticking = false;
 var $siteHeader = $('#site-header'),
 	$siteHeaderHeight = $siteHeader.outerHeight();
 
-function doSomething(scroll_pos) {
+function parallax(scroll_pos) {
   var wScrol = $(document).scrollTop();
   //$siteHeader.css('transform', 'translateY('+scroll_pos+')');
   $siteHeader.css('top', scroll_pos*-0.5);
-  console.log(scroll_pos);
 }
+
+var $leftSidebar = $('#left-sidebar'),
+	leftSidebarOffset = $leftSidebar.offset().top - 20;
+	leftSidebarScrollUntil = 2500 - leftSidebarOffset + 20;
+
 
 window.addEventListener('scroll', function(e) {
   last_known_scroll_position = window.scrollY;
 	if (last_known_scroll_position >= 0 && last_known_scroll_position < $siteHeaderHeight) {
 		if (!ticking) {
 			window.requestAnimationFrame(function() {
-				doSomething(last_known_scroll_position);
+				parallax(last_known_scroll_position);
 				ticking = false;
 			});
 		}
 		ticking = true;
+	}
+	if (last_known_scroll_position >= leftSidebarOffset && last_known_scroll_position < 2500) {
+		if (!ticking) {
+			window.requestAnimationFrame(function() {
+				$leftSidebar.addClass('left-sidebar--fixed-js');
+				$leftSidebar.css('top', 20);
+				ticking = false;
+			});
+		} 
+		ticking = true;
+	} else if (last_known_scroll_position >= 2500) {
+		$leftSidebar.removeClass('left-sidebar--fixed-js');
+		$leftSidebar.css('top', leftSidebarScrollUntil);
+	} else {
+		$leftSidebar.removeClass('left-sidebar--fixed-js');
+		$leftSidebar.css('top', 75);
 	}
 });
 
@@ -43,3 +63,6 @@ function changeToImgC() {
 function changeToImgD() {
 	$siteHeader.css('background-image', 'url(./css/img/banner-fuji.jpg)');
 }
+
+
+
